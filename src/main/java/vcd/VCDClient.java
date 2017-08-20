a00d2ccea88e4ec357c9987793ad5f887786bf29
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-08-20 18:35:08 +0200

    Prepare benchmark

--- src/main/java/vcd/VCDClient.java
+++ src/main/java/vcd/VCDClient.java
@@ -1,7 +1,9 @@
 package vcd;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
@@ -9,15 +11,42 @@ import java.util.List;
  */
 public class VCDClient {
 
+    public static Integer MESSAGE_NUMBER = 100;
+    public static List<String> MESSAGES = Arrays.asList("A", "B"); // 50% conflicts
+
     public static void main(String[] args) throws Exception {
+        Thread.sleep(3000);
+
         VCDConfig config = VCDConfig.parseArgs(args);
         VCDSocket socket = VCDSocket.create(config);
 
-        List<String> messages = Arrays.asList("ola", ",", "tudo", "bem", "?");
-        for (String message : messages) {
+        List<Long> latency = new ArrayList<>();
+
+        for (int i = 0; i < MESSAGE_NUMBER; i++) {
+            String message = nextMessage();
+
+            Long start = System.nanoTime();
             socket.send(message);
-            System.out.println("SENT:     " + message);
-            System.out.println("RECEIVED: " + socket.receive());
+            socket.receive();
+            Long end = System.nanoTime();
+
+            latency.add(end - start);
         }
+
+        Long latencyAverage = average(latency);
+        System.out.println("Latency: " + latencyAverage);
+    }
+
+    private static String nextMessage() {
+        int index = ThreadLocalRandom.current().nextInt(MESSAGES.size());
+        return MESSAGES.get(index);
+    }
+
+    private static Long average(List<Long> latency) {
+        Long sum = 0L;
+        for (Long l : latency) {
+            sum += l;
+        }
+        return sum / latency.size();
     }
 }
