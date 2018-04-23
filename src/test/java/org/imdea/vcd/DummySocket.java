0ae3e9d65f75d2b36e10131341eb2f5fa56cfdc5
otrack <0track@gmail.com>
2018-04-23 18:17:18 +0200

    better dummy socket

--- src/test/java/org/imdea/vcd/DummySocket.java
+++ src/test/java/org/imdea/vcd/DummySocket.java
@@ -1,42 +1,72 @@
 package org.imdea.vcd;
 
 import org.imdea.vcd.pb.Proto;
+import org.imdea.vcd.pb.Proto.MessageSet;
 
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class DummySocket extends Socket{
 
     BlockingQueue<Proto.MessageSet> queue;
+    Executor executor;
 
     private DummySocket(DataRW rw) {
         super(rw);
         queue = new LinkedBlockingDeque<>();
+        executor = Executors.newCachedThreadPool();
     }
 
     public static Socket create(Config config) throws IOException {
         return new DummySocket(null);
     }
 
-    public void send(Proto.MessageSet messageSet) throws IOException {
-        Proto.MessageSet.Builder builder = Proto.MessageSet.newBuilder();
+    public void send(final Proto.MessageSet messageSet) throws IOException {
 
-        for (Proto.Message message : messageSet.getMessagesList()) {
-            builder.addMessages(message);
-        }
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                MessageSet.Builder builder = MessageSet.newBuilder();
+                for (Proto.Message msg : messageSet.getMessagesList()){
+                    builder.addMessages(msg);
+                    builder.setStatus(MessageSet.Status.COMMITTED);
+                }
+                try {
+                    queue.put(builder.build());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        //Proto.MessageSet.Builder cloned = messageSet.toBuilder().clone();
-
-        try {
-            builder.setStatus(Proto.MessageSet.Status.COMMITTED);
-            queue.put(builder.build());
-
-            builder.setStatus(Proto.MessageSet.Status.DELIVERED);
-            queue.put(builder.build());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                MessageSet.Builder builder = MessageSet.newBuilder();
+                for (Proto.Message msg : messageSet.getMessagesList()) {
+                    builder.addMessages(msg);
+                    builder.setStatus(MessageSet.Status.DELIVERED);
+                }
+                try {
+                    queue.put(builder.build());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public Proto.MessageSet receive() throws IOException {
