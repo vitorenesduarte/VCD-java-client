dcf9d91f68232b8120964456a8cc70a81e2e518b
Vitor Enes <vitorenesduarte@gmail.com>
2018-05-01 15:13:37 +0100

    Read from socket and delivery in parallel

--- src/main/java/org/imdea/vcd/DataRW.java
+++ src/main/java/org/imdea/vcd/DataRW.java
@@ -18,7 +18,10 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,106 +29,165 @@ import java.util.concurrent.TimeUnit;
  */
 public class DataRW {
 
-    private static final int METRICS_REPORT_PERIOD=10; // in seconds.
-    private final MetricRegistry metrics;
-    private final Timer toAdd;
-    private final Timer createBox;
-    private final Timer tryDeliver;
-    private final Timer sorting;
-
     private final DataInputStream in;
     private final DataOutputStream out;
-    private DependencyQueue<CommitDepBox> queue;
+    private final LinkedBlockingQueue<MessageSet> toClient;
 
     public DataRW(DataInputStream in, DataOutputStream out, Integer nodeNumber) {
         this.in = in;
         this.out = out;
+        this.toClient = new LinkedBlockingQueue<>();
+    }
 
-        metrics = new MetricRegistry();
-        Set<MetricAttribute> disabledMetricAttributes  =
-                new HashSet<>(Arrays.asList(new MetricAttribute[]{
-                        MetricAttribute.MAX, MetricAttribute.STDDEV,
-                        MetricAttribute.M1_RATE, MetricAttribute.M5_RATE,
-                        MetricAttribute.M15_RATE, MetricAttribute.MIN,
-                        MetricAttribute.P99, MetricAttribute.P50,
-                        MetricAttribute.P75, MetricAttribute.P95,
-                        MetricAttribute.P98, MetricAttribute.P999}));
-        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
-                .convertRatesTo(TimeUnit.SECONDS)
-                .convertDurationsTo(TimeUnit.MICROSECONDS)
-                .disabledMetricAttributes(disabledMetricAttributes)
-                .build();
-        reporter.start(METRICS_REPORT_PERIOD, TimeUnit.SECONDS);
-
-        createBox = metrics.timer(MetricRegistry.name(DataRW.class,"createBox"));
-        toAdd = metrics.timer(MetricRegistry.name(DataRW.class,"toAdd"));
-        tryDeliver = metrics.timer(MetricRegistry.name(DataRW.class,"tryDeliver"));
-        sorting = metrics.timer(MetricRegistry.name(DataRW.class,"sorting"));
+    public void start() {
+        SocketReader reader = new SocketReader(this.in, this.toClient);
+        reader.start();
     }
 
     public void write(MessageSet messageSet) throws IOException {
         byte[] data = messageSet.toByteArray();
-        out.writeInt(data.length);
-        out.write(data, 0, data.length);
-        out.flush();
+        this.out.writeInt(data.length);
+        this.out.write(data, 0, data.length);
+        this.out.flush();
     }
 
-    public MessageSet read() throws IOException {
-        MessageSet result = null;
-        while (result == null) {
-            result = doRead();
+    public MessageSet read() throws IOException, InterruptedException {
+        return this.toClient.take();
+    }
+
+    // socket reader -> client | deliverer
+    // deliverer -> client
+    private class SocketReader extends Thread {
+
+        private final Logger LOGGER = VCDLogger.init(SocketReader.class);
+
+        private final DataInputStream in;
+        private final LinkedBlockingQueue<MessageSet> toClient;
+        private final LinkedBlockingQueue<Reply> toDeliverer;
+
+        public SocketReader(DataInputStream in, LinkedBlockingQueue<MessageSet> toClient) {
+            this.in = in;
+            this.toClient = toClient;
+            this.toDeliverer = new LinkedBlockingQueue<>();
         }
-        return result;
+
+        @Override
+        public void run() {
+            // start deliverer
+            Deliverer deliverer = new Deliverer(this.toClient, this.toDeliverer);
+            deliverer.start();
+
+            try {
+                while (true) {
+                    int length = in.readInt();
+                    byte data[] = new byte[length];
+                    in.readFully(data, 0, length);
+                    Reply reply = Reply.parseFrom(data);
+
+                    // if durable notification, send it to client
+                    // otherwise to dep queue thread
+                    switch (reply.getReplyCase()) {
+                        case SET:
+                            toClient.put(reply.getSet());
+                            break;
+                        default:
+                            toDeliverer.put(reply);
+                            break;
+                    }
+                }
+            } catch (IOException | InterruptedException e) {
+                LOGGER.log(Level.SEVERE, e.toString(), e);
+            }
+        }
     }
 
-    private MessageSet doRead() throws IOException {
-        int length = in.readInt();
-        byte data[] = new byte[length];
-        in.readFully(data, 0, length);
+    private class Deliverer extends Thread {
 
-        long start;
+        private final Logger LOGGER = VCDLogger.init(Deliverer.class);
 
-        // check if reply is a message set
-        // if yes, return it,
-        // otherwise feed the dependency queue
-        // with the commit notification
-        Reply reply = Reply.parseFrom(data);
-        switch (reply.getReplyCase()) {
-            case INIT:
-                this.queue = new DependencyQueue(Clock.eclock(reply.getInit().getCommittedMap()));
-                return null;
-            case SET:
-                return reply.getSet();
-            case COMMIT:
-                final Timer.Context createBoxContext = toAdd.time();
-                CommitDepBox box = new CommitDepBox(reply.getCommit());
-                createBoxContext.stop();
+        private final LinkedBlockingQueue<MessageSet> toClient;
+        private final LinkedBlockingQueue<Reply> toDeliverer;
+        private DependencyQueue<CommitDepBox> queue;
 
-                final Timer.Context toAddContext = createBox.time();
-                queue.add(box);
-                toAddContext.stop();
+        // metrics
+        private final int METRICS_REPORT_PERIOD = 10; // in seconds.
+        private final MetricRegistry metrics;
+        private final Timer toAdd;
+        private final Timer createBox;
+        private final Timer tryDeliver;
+        private final Timer sorting;
 
-                final Timer.Context tryDeliverContext = tryDeliver.time();
-                List<CommitDepBox> toDeliver = queue.tryDeliver();
-                tryDeliverContext.stop();
+        public Deliverer(LinkedBlockingQueue<MessageSet> toClient, LinkedBlockingQueue<Reply> toDeliverer) {
+            this.toClient = toClient;
+            this.toDeliverer = toDeliverer;
 
-                if (toDeliver.isEmpty()) {
-                    return null;
-                } else {
-                    final Timer.Context sortingContext = sorting.time();
-                    MessageSet.Builder builder = MessageSet.newBuilder();
-                    for (CommitDepBox boxToDeliver : toDeliver) {
-                        for (Message message : boxToDeliver.sortMessages()) {
-                            builder.addMessages(message);
-                        }
+            metrics = new MetricRegistry();
+            Set<MetricAttribute> disabledMetricAttributes
+                    = new HashSet<>(Arrays.asList(new MetricAttribute[]{
+                MetricAttribute.MAX, MetricAttribute.STDDEV,
+                MetricAttribute.M1_RATE, MetricAttribute.M5_RATE,
+                MetricAttribute.M15_RATE, MetricAttribute.MIN,
+                MetricAttribute.P99, MetricAttribute.P50,
+                MetricAttribute.P75, MetricAttribute.P95,
+                MetricAttribute.P98, MetricAttribute.P999}));
+            ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                    .convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MICROSECONDS)
+                    .disabledMetricAttributes(disabledMetricAttributes)
+                    .build();
+            reporter.start(METRICS_REPORT_PERIOD, TimeUnit.SECONDS);
+
+            createBox = metrics.timer(MetricRegistry.name(DataRW.class, "createBox"));
+            toAdd = metrics.timer(MetricRegistry.name(DataRW.class, "toAdd"));
+            tryDeliver = metrics.timer(MetricRegistry.name(DataRW.class, "tryDeliver"));
+            sorting = metrics.timer(MetricRegistry.name(DataRW.class, "sorting"));
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Reply reply = toDeliverer.take();
+
+                    switch (reply.getReplyCase()) {
+                        case INIT:
+                            this.queue = new DependencyQueue(Clock.eclock(reply.getInit().getCommittedMap()));
+                            break;
+                        case COMMIT:
+                            final Timer.Context createBoxContext = toAdd.time();
+                            CommitDepBox box = new CommitDepBox(reply.getCommit());
+                            createBoxContext.stop();
+
+                            final Timer.Context toAddContext = createBox.time();
+                            queue.add(box);
+                            toAddContext.stop();
+
+                            final Timer.Context tryDeliverContext = tryDeliver.time();
+                            List<CommitDepBox> toDeliver = queue.tryDeliver();
+                            tryDeliverContext.stop();
+
+                            if (!toDeliver.isEmpty()) {
+                                final Timer.Context sortingContext = sorting.time();
+                                MessageSet.Builder builder = MessageSet.newBuilder();
+                                for (CommitDepBox boxToDeliver : toDeliver) {
+                                    for (Message message : boxToDeliver.sortMessages()) {
+                                        builder.addMessages(message);
+                                    }
+                                }
+                                builder.setStatus(MessageSet.Status.DELIVERED);
+                                MessageSet messageSet = builder.build();
+                                sortingContext.stop();
+
+                                toClient.put(messageSet);
+                            }
+                            break;
+                        default:
+                            throw new RuntimeException("Reply type not supported:" + reply.getReplyCase());
                     }
-                    builder.setStatus(MessageSet.Status.DELIVERED);
-                    MessageSet messageSet = builder.build();
-                    sortingContext.stop();
-                    return messageSet;
                 }
-            default:
-                throw new RuntimeException("Reply type not supported:" + reply.getReplyCase());
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.SEVERE, e.toString(), e);
+            }
         }
     }
 
