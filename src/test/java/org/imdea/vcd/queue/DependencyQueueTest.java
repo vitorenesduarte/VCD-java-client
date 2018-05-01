03c056c5bce8d698c6975faf50e76c0742fc81b9
Vitor Enes <vitorenesduarte@gmail.com>
2018-05-01 11:57:40 +0100

    Remove apache commons dependency

--- src/test/java/org/imdea/vcd/queue/DependencyQueueTest.java
+++ src/test/java/org/imdea/vcd/queue/DependencyQueueTest.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.lang3.tuple.Pair;
 import org.imdea.vcd.Generator;
 import org.imdea.vcd.pb.Proto.Message;
 import org.imdea.vcd.queue.clock.Clock;
